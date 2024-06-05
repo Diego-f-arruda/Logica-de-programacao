5c6ea5e11e77621947e86c2c5707bef8d7b0c541
Diego-f-arruda <diego_f_arruda@estudante.sesisenai.org.br>
2024-06-04 21:19:52 -0300

    Exercicios completados

---
+++ exercicios/src/exercicios/exercicio6.java
@@ -0,0 +1,16 @@
+
+package exercicios;
+
+public class exercicio6 {
+    public static void main (String[] args){
+        int A = 10;
+        int B = 20;
+        
+        int aux = A;
+        A = B;
+        B = aux;
+        
+        System.out.println("VALOR DE A: " + A);
+        System.out.println("VALOR DE B: " + B);
+    }
+}

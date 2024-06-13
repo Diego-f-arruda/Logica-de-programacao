f6408e141bdeb172c1fd2fc8cab8a33f983147bb
Diego-f-arruda <diego_f_arruda@estudante.sesisenai.org.br>
2024-06-12 21:44:15 -0300

    Correcao exercicios dia 12/06

--- 02aulaEstruturasCondocionais/src/SalarioFamilia.js
+++ 02aulaEstruturasCondocionais/src/SalarioFamilia.js
@@ -1,27 +1,28 @@
 import leia from "readline-sync"
 
+var Nome = leia.question("Informe o seu Nome: ")
 var Salario = leia.questionFloat("Informe o salario Fixo: ")
 var DEP = leia.questionInt("Informe quantos dependentes possui: ")
 
 switch(DEP){
     case 0:
-        var SalC = Salario + 0.02
-        console.log("Seu salario ficara R$" + SalC)
+        var SalC = Salario + (Salario * 0.02)
+        console.log("Seu salario ficara R$" + SalC.toFixed(2))
         break;
     case 1:
-        var SalC = Salario + 0.05
-        console.log("Seu salario ficara R$" + SalC)
+        var SalC = Salario + (Salario *  0.05)
+        console.log("Seu salario ficara R$" + SalC.toFixed(2))
         break;
     case 2:
-        var SalC = Salario + 0.07
-        console.log("Seu salario ficara R$" + SalC)
+        var SalC = Salario + (Salario * 0.07)
+        console.log("Seu salario ficara R$" + SalC.toFixed(2))
         break;
     case 3:
-        var SalC = Salario + 0.1
-        console.log("Seu salario ficara R$" + SalC)
+        var SalC = Salario + (Salario * 0.1)
+        console.log("Seu salario ficara R$" + SalC.toFixed(2))
         break;
     default:
-        var SalC = Salario + 0.15
-        console.log("Seu salario ficara R$" + SalC)
+        var SalC = Salario + (Salario * 0.15)
+        console.log("Seu salario ficara R$" + SalC.toFixed(2))
         break;
 }
